93ced22790ecc2214b13dc61ffc7fabf3c562f82
gammaled <guleduni@yahoo.co.uk>
2021-12-09 17:37:15 +0000

    created deployment script for UCHI token

---
+++ scripts/1_deploy_uchi_token.py
@@ -0,0 +1,9 @@
+from brownie import accounts, config, UchihaToken
+
+initial_supply = 100000000000000000000
+token_name = "UCHIHA"
+token_symbol = "UCHI"
+
+def main():
+    account = accounts.add(config["wallets"]["from_key"])
+    erc20 = UchihaToken.deploy({"from": account})
