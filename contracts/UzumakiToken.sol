28bcaaf6da863b534712f155bc1fba2991033f7b
gammaled <guleduni@yahoo.co.uk>
2021-12-09 16:47:11 +0000

    removed redundant parameter in constructor function

--- contracts/UzumakiToken.sol
+++ contracts/UzumakiToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.6.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract UzumakiToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Uzumaki", "UZU", 18) public {
+    constructor(uint256 initialSupply) ERC20("Uzumaki", "UZU") public {
         _mint(msg.sender, initialSupply);
     }
 }
